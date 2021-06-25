1e1ca34de2bddb3290a9550371ae4c662a4db8c4
Alexandr Siryj <theobroma222@gmail.com>
2021-06-25 07:11:35 +0300

    pictures reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,14 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { PicturesState, picturesReducer } from './pictures';
 
 export interface State {
-
+  pictures:PicturesState
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  pictures:picturesReducer
 };
 
 
---
+++ src/app/reducers/pictures.ts
@@ -0,0 +1,45 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+export const clear = createAction('[COUNTER] clear');
+
+// const picturesInitialState = {
+//   data: {
+//     total: 0,
+//     totalHits: 0,
+//     hits: [],
+//   } as PicturesDataType,
+//   loading: false,
+//   page: 1,
+//   pictureSearch: 'nature',
+//   largeImageURL: '',
+//   tags: '' as string,
+// };
+
+export interface PicturesState {
+  data: any,
+  loading: boolean,
+  page: number,
+  pictureSearch: string,
+  largeImageURL: string,
+  tags:string,
+}
+
+export const initialState: PicturesState = {
+  data: {
+    total: 0,
+    totalHits: 0,
+    hits: [],
+  } ,
+  loading: false,
+  page: 1,
+  pictureSearch: 'nature',
+  largeImageURL: '',
+  tags: '' ,
+};
+
+export const picturesReducer = createReducer(
+  initialState,
+  on(clear, state => ({
+    ...state,
+    data: []
+  }))
+);

9c54978df1ec63ab320334625d39e58e157a4066
Alexandr Siryj <theobroma222@gmail.com>
2021-06-27 11:03:55 +0300

    theme select logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { ThemeService } from './core/services/theme.service';
+import { selectEffectiveTheme } from './reducers/settings/settings.selectors';
+
 
 @Component({
   selector: 'app-root',
@@ -9,13 +13,22 @@ import { ThemeService } from './core/services/theme.service';
 })
 export class AppComponent {
   isDarkTheme: Observable<boolean>;
+  theme$: Observable<string>;
 
-  constructor(private themeService: ThemeService) {
+  constructor(private themeService: ThemeService,private store: Store<any>) {
     // Initialization inside the constructor
     this.isDarkTheme =of(false);
+    this.theme$=of("LIGHT-THEME");
  }
 
-  ngOnInit() {
-    this.isDarkTheme = this.themeService.isDarkTheme;
+  // ngOnInit() {
+  //   this.isDarkTheme = this.themeService.isDarkTheme;
+  // }
+
+  ngOnInit(): void {
+    // this.theme$ = this.store.pipe(select(selectTheme));
+    this.theme$ = this.store.pipe(select(selectEffectiveTheme)).pipe(
+      tap(val => console.log('Theme: ', val))
+    );
   }
 }
--- src/app/reducers/settings/settings.selectors.ts
+++ src/app/reducers/settings/settings.selectors.ts
@@ -8,3 +8,21 @@ export const selectSettings = createSelector(
   selectSettingsState,
   (state: SettingsState) => state
 );
+
+export const selectTheme = createSelector(
+  selectSettings,
+  (settings) => settings.theme
+);
+
+// export const selectEffectiveTheme = createSelector(
+//   selectTheme,
+//   selectNightTheme,
+//   selectIsNightHour,
+//   (theme, nightTheme, isNightHour) =>
+//     (isNightHour ? nightTheme : theme).toLowerCase()
+// );
+
+export const selectEffectiveTheme = createSelector(
+  selectTheme,
+  (theme) => theme.toLowerCase()
+);
